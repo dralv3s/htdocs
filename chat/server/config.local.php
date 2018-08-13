7229b5caff4aeebc679a25a2591526ea88c354ce
Administrator@LINKAUTOMACAO.LOCAL <Administrator@LINKAUTOMACAO.LOCAL>
2018-08-13 15:58:37 -0300

    Commit inicial, projeto rodando modo inicial

---
+++ chat/server/config.local.php
@@ -0,0 +1,14 @@
+<?php
+
+
+// Gives channel operator rights to the first connected user
+// (Default value: true)
+$GLOBALS['first_is_op'] = false;
+$params["admins"] = array('link' => 'senhadosite200', 'drocha' => 'abc@123');
+// custom user hooks
+//$GLOBALS['pfc_hooks'] = array('link' => 'senhadosite200', 'drocha' => 'abc@123');
+//$GLOBALS['pfc_hooks']['admins'] = array('link' => 'senhadosite200', 'drocha' => 'abc@123');
+
+
+
+
